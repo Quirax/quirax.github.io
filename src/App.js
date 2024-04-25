d392a91fc8508c0e3f02cbb9d550b587cc8da617
Quirax Lee <quiraxical@gmail.com>
2024-04-25 14:29:23 +0900

    Described contents of header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import './App.css'
 import { MarkdownFileRenderer } from './components/MarkdownRenderer'
 import test_md from './docs/test.md'
+import profile_image from './profile.jpg'
 
 function App() {
     return (
         <>
-            <header>헤더</header>
+            <header>
+                <div id='left_header'>
+                    <img
+                        src={profile_image}
+                        alt='킈락의 프로필 이미지'
+                    />
+                    <a href='#front'>Quirax Lee</a>
+                    <a href='#profile'>프로필</a>
+                    <a href='#footer'>푸터</a>
+                </div>
+                <div id='right_header'>
+                    <button>언어 선택</button>
+                    <button>맨 위로</button>
+                </div>
+            </header>
             <main>
-                <section>처음</section>
-                <section>
+                <section id='front'>
+                    <img
+                        src={profile_image}
+                        alt='킈락의 프로필 이미지'
+                    />
+                </section>
+                <section id='profile'>
                     <MarkdownFileRenderer src={test_md} />
                 </section>
             </main>
-            <footer>푸터</footer>
+            <footer id='footer'>푸터</footer>
         </>
     )
 }
