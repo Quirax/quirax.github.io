768c58db94abe1a94fdf2bebf52cc33b2e689ba0
Quirax Lee <quiraxical@gmail.com>
2024-04-25 13:45:22 +0900

    Described skeleton

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import { MarkdownFileRenderer } from './components/MarkdownRenderer'
 import test_md from './docs/test.md'
 
 function App() {
-    return <MarkdownFileRenderer src={test_md} />
+    return (
+        <>
+            <header>헤더</header>
+            <main>
+                <section>처음</section>
+                <section>
+                    <MarkdownFileRenderer src={test_md} />
+                </section>
+            </main>
+            <footer>푸터</footer>
+        </>
+    )
 }
 
 export default App
