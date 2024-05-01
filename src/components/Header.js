8d5325e40e50dd008493afa7850dd174486348c1
Quirax Lee <quiraxical@gmail.com>
2024-05-01 12:22:18 +0900

    Replaced css of header to styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,112 @@
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
+
 import { ProfileImg } from './ProfileImg'
 
-import './Header.css'
+const StyledHeader = styled.header`
+    height: var(--header-height);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    position: fixed;
+    width: 100%;
+    background: var(--header-background);
+    color: var(--header-color);
+`
+
+const Div = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
+const Button = styled.button`
+    height: 100%;
+    padding: var(--header-padding);
+    box-sizing: border-box;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    appearance: none;
+    color: inherit;
+    text-align: center;
+    cursor: pointer;
+    background: inherit;
+    border: none;
+    font: inherit;
+    text-decoration: none;
+    width: var(--header-height);
+`
+
+const A = styled.a`
+    height: 100%;
+    padding: var(--header-padding);
+    box-sizing: border-box;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    appearance: none;
+    color: inherit;
+    text-align: center;
+    cursor: pointer;
+    background: inherit;
+    border: none;
+    font: inherit;
+    text-decoration: none;
+    width: var(--header-height);
+`
+
+const Link = styled(A)`
+    width: var(--header-button-width);
+    font-size: var(--header-button-font-size);
+
+    &:hover, &:active {
+        background: var(--header-active-background);
+        color: var(--header-active-color);
+    }
+`
+
+const MenuButton = styled(Button)`
+    width: var(--header-button-width);
+    font-size: var(--header-button-font-size);
+
+    &:hover, &:active {
+        background: var(--header-active-background);
+        color: var(--header-active-color);
+    }
+`
 
 export function Header({ ...props }) {
     const { t } = useTranslation()
 
     return (
-        <header>
-            <div id='left_header'>
-                <button>
+        <StyledHeader>
+            <Div id='left_header'>
+                <Button>
                     <span className='material-icons'>menu</span>
-                </button>
-                <a href='#top'>
+                </Button>
+                <A href='#top'>
                     <ProfileImg />
-                </a>
-                <a href='#profile'>
+                </A>
+                <Link href='#profile'>
                     <span>{t('profile')}</span>
-                </a>
-                <a href='#portfolio'>
+                </Link>
+                <Link href='#portfolio'>
                     <span>{t('portfolio')}</span>
-                </a>
-                <a href='#project'>
+                </Link>
+                <Link href='#project'>
                     <span>{t('project')}</span>
-                </a>
-            </div>
-            <div id='right_header'>
-                <button>
+                </Link>
+            </Div>
+            <Div id='right_header'>
+                <MenuButton>
                     <span>언어 선택</span>
-                </button>
-                <a href='#top'>
-                    <span>
-                        <span className='material-icons'>keyboard_double_arrow_up</span>
-                    </span>
-                </a>
-            </div>
-        </header>
+                </MenuButton>
+                <A href='#top'>
+                    <span className='material-icons'>keyboard_double_arrow_up</span>
+                </A>
+            </Div>
+        </StyledHeader>
     )
 }
--- src/components/ProfileImg.js
+++ src/components/ProfileImg.js
@@ -1,11 +1,16 @@
 import { useTranslation } from 'react-i18next'
 import profile_image from '../profile.png'
+import styled from 'styled-components'
+
+const Img = styled.img`
+    height: 100%;
+`
 
 export function ProfileImg() {
     const { t } = useTranslation()
 
     return (
-        <img
+        <Img
             src={profile_image}
             alt='킈락의 프로필 이미지'
         />
