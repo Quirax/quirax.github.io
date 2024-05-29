b5ae175877513a8a5ffb86ec79c858377586c2c2
Quirax Lee <quiraxical@gmail.com>
2024-05-29 12:47:18 +0900

    Refactored ContentSection.js to move styled Section and H2 to common.js

--- src/components/common.js
+++ src/components/common.js
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import { headerHeight } from '../GlobalStyle'
 
 export const MaterialIcon = ({ children }) => <span className='material-icons'>{children}</span>
 
@@ -21,3 +22,19 @@ export const buttonStyle = css`
 `
 
 export const InlinedButton = styled.button(buttonStyle)
+
+export const Section = styled.section`
+    margin: var(--content-margin);
+
+    &:before {
+        content: "";
+        display: block;
+        height: ${headerHeight}px; /* fixed header height*/
+        margin: calc(var(--content-margin) - ${headerHeight}px) 0 0; /* negative fixed header height */
+    }
+`
+
+export const H2 = styled.h2`
+    font-size: var(--content-title-font-size);
+    color: var(--content-title-color);
+`
